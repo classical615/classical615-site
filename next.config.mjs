170d6e6bbfa261c4a959e581bafff6a3c35868bf
Kaitlyn Raitz <kaitlynraitz@Kaitlyns-MacBook-Pro.local>
2026-08-21 10:50:06 -0500

    Fix chromium files for scraper

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/@sparticuz/chromium/**/*'],
+  },
 };
 
 export default nextConfig;
