dd51931f2b0d3d33f0047f0256a791965beaa3d3
Kaitlyn Raitz <kaitlynraitz@Kaitlyns-MacBook-Pro.local>
2026-08-21 10:52:46 -0500

    Fix chromium external packages config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
+  },
   outputFileTracingIncludes: {
     '/api/**/*': ['./node_modules/@sparticuz/chromium/**/*'],
   },
